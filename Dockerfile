97540169e5ef2f9bd2fe108a15c17e4b817ccfad
affian.onn <affianonn@hotmail.com>
2025-05-04 16:50:53 +0800

    Init commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM golang:latest AS build
+LABEL maintainer="Affian Onn <affianonn@hotmail.com>"
+
+WORKDIR /compose/hello-docker
+
+COPY src/hello-world.go main.go
+
+RUN go mod init go-nica && go mod tidy
+RUN go get github.com/shirou/gopsutil/cpu
+RUN CGO_ENABLED=0 go build -o hello main.go
+
+FROM scratch
+COPY --from=build /compose/hello-docker/hello /usr/local/bin/hello
+
+EXPOSE 8081
+CMD ["/usr/local/bin/hello"]
---
+++ src/hello-world.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"runtime"
+	"time"
+
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/mem"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println(r.URL.RawQuery)
+	fmt.Fprint(w, `
+			##         .
+		## ## ##        ==
+	## ## ## ## ##    ===
+	/"""""""""""""""""\___/ ===
+	{                       /  ===-
+	\______ O           __/
+	\    \         __/
+	\____\_______/
+		
+	Hello World from Docker and Kubernetes! v1.0.3
+	`)
+
+	cpuUsage, err := cpu.Percent(time.Second, false)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	memUsage, err := mem.VirtualMemory()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprint(w, "\n")
+	fmt.Fprint(w, "OS Utilisation:\n")
+	fmt.Fprintf(w, "Current CPU Usage: %.2f%%\n", cpuUsage[0])
+	fmt.Fprintf(w, "Number of CPUs: %d\n", runtime.NumCPU())
+	fmt.Fprintf(w, "Number of goroutines: %d\n", runtime.NumGoroutine())
+
+	fmt.Fprintf(w, "Current Memory Usage: %.2f%%", memUsage.UsedPercent)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+
+	go func() {
+		for {
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	fmt.Println("Server listening on port 8081")
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
